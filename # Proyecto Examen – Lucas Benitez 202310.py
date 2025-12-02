dd5e4256041b4f490a4864f69af3309d0b78bddd
lucasdeveloperr <benitezsilvalucastobias@gmail.com>
2025-12-02 20:50:09 -0300

    Tema 1

--- # Proyecto Examen – Lucas Benitez 202310.py
+++ # Proyecto Examen – Lucas Benitez 202310.py
@@ -1,15 +1,8 @@
 # Proyecto Examen – Lucas Benitez 2023100851
 
-Este repositorio contiene dos carpetas principales:
+
 
 - **Tema1**
 - **Tema2**
 
-Dentro de este repositorio se desarrollarán los ejercicios solicitados.
 
-## Cómo subir la página al servidor
-
-1. Abrir la terminal en VS Code o Git Bash.  
-2. Ejecutar `git add .` para agregar los archivos.  
-3. Ejecutar `git commit -m "Actualización del proyecto"`.  
-4. Ejecutar `git push` para subir los cambios a GitHub.
